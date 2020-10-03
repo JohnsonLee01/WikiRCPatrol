3cecab180dbd965e7f7aecba6892c5669d8c691b
JohnsonLee01 <65768259+JohnsonLee01@users.noreply.github.com>
2020-10-03 23:50:29 +0700

    Update keyCombinations.js

--- src/js/keyCombinations.js
+++ src/js/keyCombinations.js
@@ -1,45 +1,46 @@
-/**
- * Page for processing key combinations
- */
+    /**
+     * Trang xử lý các tổ hợp phím
+     */
 
-$(document).keydown(function (e) {
-    if (e.ctrlKey && e.altKey) {
-        switch (e.which) {
-            case 82: e.preventDefault(); //rollback (r)
-                rcpatrol.revert(rcpatrol.changes[rcpatrol.currentChange].title, rcpatrol.changes[rcpatrol.currentChange].user, function () {
-                    rcpatrol.currentChange++;
-                    rcpatrol.loadChange(rcpatrol.changes[rcpatrol.currentChange]);
-                },
-                    function () {
+    $(document).keydown(function (e) {
+        if (e.ctrlKey && e.altKey) {
+            switch (e.which) {
+                case 82: e.preventDefault(); //rollback (r)
+                    rcpatrol.revert(rcpatrol.changes[rcpatrol.currentChange].title, rcpatrol.changes[rcpatrol.currentChange].user, function () {
+                        rcpatrol.currentChange++;
                         rcpatrol.loadChange(rcpatrol.changes[rcpatrol.currentChange]);
-                    });
-                break;
-            case 32: e.preventDefault(); //next edit ( )
-                rcpatrol.nexteditbutton.$element.click();
-                break;
-            case 188: e.preventDefault(); //previous edit (<)
-                rcpatrol.previouseditbutton.$element.click();
-                break;
-            case 187: e.preventDefault(); //thanks (+)
-                rcpatrol.thankbutton.$element.click();
-                break;
-            default:
-                for (var option of rcpatrol.dropdown) {
-                    if (option.keycode == e.which) {
-                        rcpatrol.rcpatrolbox.setValue(option.summary);
-                        rcpatrol.revert(rcpatrol.changes[rcpatrol.currentChange].title, rcpatrol.changes[rcpatrol.currentChange].user, function () {
-                            rcpatrol.warn(rcpatrol.changes[rcpatrol.currentChange].user, option.template, rcpatrol.changes[rcpatrol.currentChange].title);
-                            rcpatrol.currentChange++;
-                            rcpatrol.loadChange(rcpatrol.changes[rcpatrol.currentChange]);
-                        }, function () {
+                    },
+                        function () {
                             rcpatrol.loadChange(rcpatrol.changes[rcpatrol.currentChange]);
                         });
-                        break;
-                    } else {
-                        continue;
+                    break;
+                case 32: e.preventDefault(); //next edit ( )
+                    rcpatrol.nexteditbutton.$element.click();
+                    break;
+                case 188: e.preventDefault(); //previous edit (<)
+                    rcpatrol.previouseditbutton.$element.click();
+                    break;
+                case 187: e.preventDefault(); //thanks (+)
+                    rcpatrol.thankbutton.$element.click();
+                    break;
+                default:
+                    for (var option of rcpatrol.dropdown) {
+                        if (option.keycode == e.which) {
+                            rcpatrol.rcpatrolbox.setValue(option.summary);
+                            rcpatrol.revert(rcpatrol.changes[rcpatrol.currentChange].title, rcpatrol.changes[rcpatrol.currentChange].user, function () {
+                                rcpatrol.warn(rcpatrol.changes[rcpatrol.currentChange].user, option.template, rcpatrol.changes[rcpatrol.currentChange].title);
+                                rcpatrol.currentChange++;
+                                rcpatrol.loadChange(rcpatrol.changes[rcpatrol.currentChange]);
+                            }, function () {
+                                rcpatrol.loadChange(rcpatrol.changes[rcpatrol.currentChange]);
+                            });
+                            break;
+                        } else {
+                            continue;
+                        }
                     }
-                }
-                break;
+                    break;
+            }
         }
-    }
+    });
 });
