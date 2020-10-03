ea6a35d9517382f7febe1e6d0e17789136403c30
JohnsonLee01 <65768259+JohnsonLee01@users.noreply.github.com>
2020-10-03 23:48:45 +0700

    Update i18n.js

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -1,96 +1,96 @@
-/**
- * Internationalization here
- */
-rcpatrol.i18n = {
-    reasontorollback: {
-        en: 'Reason to rollback (optional)'
-    },
-    rollback: {
-        en: 'Rollback'
-    },
-    rollingback: {
-        en: 'Rolling back...'
-    },
-    rollbackfailed: {
-        en: 'Rollback failed'
-    },
-    rollbacksuccess: {
-        en: 'Rollback complete'
-    },
-    rollbacksummary: {
-        en: '[[w:User:Awesome Aasim/rcpatrol|RCP]] reverted edits by [[Special:Contributions/$2|$2]] ([[User_talk:$2|Talk]]); changed back to last revision by [[Special:Contributions/$1|$1]]'
-    },
-    warnsummary: {
-        en: '[[w:User:Awesome Aasim/rcpatrol|RCP]] send warning to $1 about [[$2]]'
-    },
-    reportsummary: {
-        en: '[[w:User:Awesome Aasim/rcpatrol|RCP]] report $1'
-    },
-    rollbacktitle: {
-        en: "Revert this user's edits",
-    },
-    previousedit: {
-        en: 'Previous edit'
-    },
-    previousedittitle: {
-        en: 'Load the previous edit in the batch'
-    },
-    nextedit: {
-        en: 'Next edit'
-    },
-    nextedittitle: {
-        en: 'Load the next edit in the batch'
-    },
-    refresh: {
-        en: 'Refresh'
-    },
-    connectionlost: {
-        en: 'Lost connection'
-    },
-    connectionlostdiffmessage: {
-        en: "Could not load diff.  Please check your Internet connection.  The diff will automatically reload when the connection is reestablished."
-    },
-    thank: {
-        en: 'Thank'
-    },
-    thanktitle: {
-        en: "Thank this user for their edits"
-    },
-    rollbackandwarn: {
-        en: 'Rollback and warn'
-    },
-    endoflist: {
-        en: 'Reached end of list.  Loading next batch...'
-    },
-    thankssent: {
-        en: 'Thanks sent!'
-    },
-    rcpatroltitle: {
-        en: 'Recent Changes Patrol'
-    },
-    rcpatroltitlewithdiff: {
-        en: 'Recent Changes Patrol "$1"'
-    },
-    delete: {
-        en: 'Delete'
-    },
-    protect: {
-        en: 'Protect'
-    },
-    block: {
-        en: 'Block poster'
-    },
-    history: {
-        en: 'View page history'
-    },
-    diff: {
-        en: 'View diff'
+    /**
+     * Bắt đầu biên dịch tại đây
+     */
+    rcpatrol.i18n = {
+        reasontorollback: {
+            vi: 'Lý do lùi sửa (tuỳ chọn)'
+        },
+        rollback: {
+            vi: 'Lùi sửa'
+        },
+        rollingback: {
+            vi: 'Đang lùi sửa...'
+        },
+        rollbackfailed: {
+            vi: 'Lùi sửa thất bại'
+        },
+        rollbacksuccess: {
+            vi: 'Lùi sửa thành công'
+        },
+        rollbacksummary: {
+            vi: '[[w:en:User:Awesome Aasim/rcpatrol|RCP]] đã lùi lại sửa đổi của [[Special:Contributions/$2|$2]] ([[User_talk:$2|thảo luận]]); quay về phiên bản cuối của [[Special:Contributions/$1|$1]]'
+        },
+        warnsummary: {
+            vi: '[[w:en:User:Awesome Aasim/rcpatrol|RCP]] gửi cảnh báo đến $1 về [[$2]]'
+        },
+        reportsummary: {
+            vi: '[[w:en:User:Awesome Aasim/rcpatrol|RCP]] báo cáo $1'
+        },
+        rollbacktitle: {
+            vi: "Lùi lại sửa đổi của thành viên này",
+        },
+        previousedit: {
+            vi: 'Sửa đổi trước đó'
+        },
+        previousedittitle: {
+            vi: 'Tải các bản sửa đổi trước đó trong danh sách'
+        },
+        nextedit: {
+            vi: 'Sửa đổi tiếp theo'
+        },
+        nextedittitle: {
+            vi: 'Tải các bản sửa đổi tiếp theo trong danh sách'
+        },
+        refresh: {
+            vi: 'Tải lại'
+        },
+        connectionlost: {
+            vi: 'Mất kết nối'
+        },
+        connectionlostdiffmessage: {
+            vi: "Không thể tải bản khác biệt. Xin vui lòng kiểm tra kết nối Internet của bạn. Bản khác biệt sẽ tự động tải lại khi kết nối Internet được thiết lập."
+        },
+        thank: {
+            vi: 'Cảm ơn'
+        },
+        thanktitle: {
+            vi: "Cảm ơn thành viên này vì các sửa đổi của họ"
+        },
+        rollbackandwarn: {
+            vi: 'Lùi sửa và cảnh báo'
+        },
+        endoflist: {
+            vi: 'Đã đến cuối danh sách. Đang tải danh sách tiếp theo...'
+        },
+        thankssent: {
+            vi: 'Đã gửi lời cảm ơn!'
+        },
+        rcpatroltitle: {
+            vi: 'Tuần tra thay đổi gần đây'
+        },
+        rcpatroltitlewithdiff: {
+            vi: 'Tuần tra thay đổi gần đây "$1"'
+        },
+        delete: {
+            vi: 'Xoá'
+        },
+        protect: {
+            vi: 'Khoá'
+        },
+        block: {
+            vi: 'Cấm'
+        },
+        history: {
+            vi: 'Xem lịch sử trang'
+        },
+        diff: {
+            vi: 'Xem khác biệt'
+        }
     }
-}
-/**
- * End of internationaliztaion.  DO NOT EDIT BELOW THIS LINE
- */
-rcpatrol.msgs = {};
-for (var i in rcpatrol.i18n) {
-    rcpatrol.msgs[i] = rcpatrol.i18n[mw.config.get("wgUserLanguage")] ? rcpatrol.i18n[mw.config.get("wgUserLanguage")] : rcpatrol.i18n["en"]; //always fall back to English if the message translation is incomplete
-}
+    /**
+     * Kết thúc quá trình biên dịch.  KHÔNG ĐƯỢC PHÉP SỬA DÒNG BÊN DƯỚI.
+     */
+    rcpatrol.msgs = {};
+    for (var i in rcpatrol.i18n) {
+        rcpatrol.msgs[i] = rcpatrol.i18n[mw.config.get("wgUserLanguage")] ? rcpatrol.i18n[mw.config.get("wgUserLanguage")] : rcpatrol.i18n["en"]; //luôn trở ngôn ngữ lại tiếng Anh nếu bản dịch thư không hoàn chỉnh
+    }
