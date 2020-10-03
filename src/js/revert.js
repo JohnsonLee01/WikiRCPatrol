770d80f253d935fca4dd8074adbc74b888543bad
JohnsonLee01 <65768259+JohnsonLee01@users.noreply.github.com>
2020-10-03 23:52:27 +0700

    Update revert.js

--- src/js/revert.js
+++ src/js/revert.js
@@ -1,49 +1,49 @@
-/**
- * Rolls back edits on diffs
- * @param {*} page the page to revert the edits on
- * @param {*} user the user to revert
- * @param {*} afterSuccess what to do after the revert is successful
- * @param {*} afterFail what to do if the revert fails
- */
-rcpatrol.revert = function (page, user, afterSuccess, afterFail) {
-    if (!rcpatrol.rcpatrolbutton.isDisabled()) {
-        var summary = rcpatrol.rcpatrolbox.getValue();
-        rcpatrol.rcpatrolbutton.setLabel("Rolling back...");
-        rcpatrol.setDisabled(true);
-        $.get(mw.config.get("wgScriptPath") + "/api.php", {
-            "action": "query",
-            "format": "json",
-            "meta": "tokens",
-            "type": "rollback"
-        }).done(function (result) {
-            if (result.error) {
-                rcpatrol.rcpatrolbutton.setLabel("Rollback failed");
-                alert(result.error.info);
-                afterFail();
-            } else {
-                $.post(mw.config.get("wgScriptPath") + "/api.php", {
-                    "action": "rollback",
-                    "format": "json",
-                    "title": page,
-                    "token": result.query.tokens.rollbacktoken,
-                    "user": user,
-                    "summary": "[[User:Awesome Aasim/rcpatrol|RCP]] reverted edits by [[Special:Contributions/$2|$2]] ([[User_talk:$2|Talk]]); changed back to last revision by [[Special:Contributions/$1|$1]]" + (summary ? ": " + summary : "")
-                }).done(function (result) {
-                    if (result.error) {
-                        rcpatrol.rcpatrolbutton.setLabel("Rollback failed");
-                        alert(result.error.info);
-                        afterFail();
-                    } else {
-                        rcpatrol.rcpatrolbutton.setLabel("Rollback complete");
-                        afterSuccess();
-                    }
-                }).fail(function () {
-                    console.log(result);
-                    alert("Lost connection.");
-                    rcpatrol.rcpatrolbutton.setLabel("Rollback failed");
+    /**
+     * Lùi lại sửa đổi trong bản khác biệt
+     * @param {*} trang để lùi lại các sửa đổi 
+     * @param {*} thành viên để lùi sửa 
+     * @param {*} sau khi thành công phải làm gì sau khi lùi sửa thành công
+     * @param {*} sau khi thất bại phải làm gì nếu lùi sửa không thành công
+     */
+    rcpatrol.revert = function (page, user, afterSuccess, afterFail) {
+        if (!rcpatrol.rcpatrolbutton.isDisabled()) {
+            var summary = rcpatrol.rcpatrolbox.getValue();
+            rcpatrol.rcpatrolbutton.setLabel("Đang lùi sửa...");
+            rcpatrol.setDisabled(true);
+            $.get(mw.config.get("wgScriptPath") + "/api.php", {
+                "action": "query",
+                "format": "json",
+                "meta": "tokens",
+                "type": "rollback"
+            }).done(function (result) {
+                if (result.error) {
+                    rcpatrol.rcpatrolbutton.setLabel("Lùi sửa thất bại");
+                    alert(result.error.info);
                     afterFail();
-                });
-            }
-        });
-    }
-};
+                } else {
+                    $.post(mw.config.get("wgScriptPath") + "/api.php", {
+                        "action": "rollback",
+                        "format": "json",
+                        "title": page,
+                        "token": result.query.tokens.rollbacktoken,
+                        "user": user,
+                        "summary": "[[User:Awesome Aasim/rcpatrol|RCP]] đã lùi lại sửa đổi của [[Special:Contributions/$2|$2]] ([[User_talk:$2|Talk]]); quay trở về phiên bản cuối của [[Special:Contributions/$1|$1]]" + (summary ? ": " + summary : "")
+                    }).done(function (result) {
+                        if (result.error) {
+                            rcpatrol.rcpatrolbutton.setLabel("Lùi sửa thất bại");
+                            alert(result.error.info);
+                            afterFail();
+                        } else {
+                            rcpatrol.rcpatrolbutton.setLabel("Lùi sửa thành công");
+                            afterSuccess();
+                        }
+                    }).fail(function () {
+                        console.log(result);
+                        alert("Lost connection.");
+                        rcpatrol.rcpatrolbutton.setLabel("Lùi sửa thất bại");
+                        afterFail();
+                    });
+                }
+            });
+        }
+    };
